0c8b5fd8a1bb539f5464770a7466104c614980a6
jolanda <thelapegna@gmail.com>
2020-10-09 16:52:26 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, {
-  useState
-} from 'react';
-import './index.css';
-import Search from './components/Search'
+import React, { useState } from "react";
+import "./index.css";
+import Search from "./components/Search";
 import axios from "axios";
-import Results from './components/Results'
-import Popup from './components/Popup.js'
-import {
-  logDOM
-} from '@testing-library/react';
+import Results from "./components/Results";
+import Popup from "./components/Popup.js";
+import { logDOM } from "@testing-library/react";
 
 function App() {
-
   const [state, setState] = useState({
     input: "",
     results: [],
-    selected: {}
-
+    selected: {},
   });
   const apiurl = "http://www.omdbapi.com/?apikey=ceb4ab38";
-
 
   const handleInput = (e) => {
     let userInput = e.target.value;
@@ -27,90 +20,65 @@ function App() {
 
     setState({
       ...state,
-      input: userInput
-    })
-  }
+      input: userInput,
+    });
+  };
 
-  const openPop = id => {
-    axios(apiurl + "&i=" + id).then(({
-      data
-    }) => {
+  const openPop = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
       let result = data;
 
-      console.log(result)
+      console.log(result);
 
-      setState(prevState => {
+      setState((prevState) => {
         return {
           ...prevState,
-          selected: result
-        }
+          selected: result,
+        };
       });
     });
-  }
+  };
 
   const closePopup = () => {
-    setState(state => {
+    setState((state) => {
       return {
         ...state,
-        selected: {}
-      }
+        selected: {},
+      };
     });
-  }
+  };
 
   const search = (e) => {
     if (e.key === "Enter") {
-      axios(apiurl + "&s=" + state.input).then(({
-        data
-      }) => {
+      axios(apiurl + "&s=" + state.input).then(({ data }) => {
         let results = data.Search;
         console.log(results, "these are the results");
-        setState(state => {
+        setState((state) => {
           return {
             ...state,
-            results: results
-          }
-        })
+            results: results,
+          };
+        });
       });
     }
-  }
+  };
 
-  return ( <
-    div className = "App" >
-    <
-    header >
-    <
-    h1 > Movie Database < /h1> <
-    /header> <
-    main >
-    <
-    Search handleInput = {
-      handleInput
-    }
-    search = {
-      search
-    }
-    />
+  return (
+    <div className="App">
+      <header>
+        <h1> Movie Database </h1>{" "}
+      </header>{" "}
+      <main>
+        <Search handleInput={handleInput} search={search} />{" "}
+        <Results results={state.results} openPop={openPop} />{" "}
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}{" "}
+      </main>{" "}
+    </div>
+  );
+}
 
-    <
-    Results results = {
-      state.results
-    }
-    openPop = {
-      openPop
-    }
-    />
-
-    {
-      (typeof state.selected.Title != "undefined") ? < Popup selected = {
-        state.selected
-      }
-      closePopup = {
-        closePopup
-      }
-      /> : false} <
-      /main> <
-      /div>
-    );
-  }
-
-  export default App;
+export default App;
